2658dc87be79ed272799733831995b143b4aee95
deadpooll312 <deadpoll312@gmail.com>
2021-08-18 20:04:43 +0600

    editable line

--- src/helpers/mapHelpers.js
+++ src/helpers/mapHelpers.js
@@ -16,3 +16,28 @@ export const initMap = ({ mapId, image }) => {
 
   return map;
 };
+
+export const addMarker = ({ map, latLng, draggable, setPosition }) => {
+  const marker = L.marker(latLng, {
+    draggable,
+  }).addTo(map);
+
+  marker.on('dragend', () => {
+    setPosition(marker.getLatLng());
+  });
+
+  marker.on('move', () => {
+    setPosition(marker.getLatLng());
+  });
+
+  return marker;
+};
+
+export const addPolyLine = ({ map, latLngs }) => {
+  const polyLine = L.polyline(latLngs, {
+    weight: 2,
+    dashArray: '5',
+  }).addTo(map);
+
+  return polyLine;
+};
--- src/pages/UploadMap/components/CRSMap/index.js
+++ src/pages/UploadMap/components/CRSMap/index.js
@@ -2,16 +2,54 @@ import React, { useEffect, useState } from 'react';
 
 import './index.scss';
 
-import { initMap } from '../../../../helpers/mapHelpers';
+import {
+  addMarker,
+  addPolyLine,
+  initMap,
+} from '../../../../helpers/mapHelpers';
+
+import svgImage from '../../../../assets/layers/pic-2.svg';
 
 const CRSMap = ({ image }) => {
   const mapId = 'map';
 
   const [map, setMap] = useState();
+  const [markerA, setMarkerA] = useState();
+  const [markerB, setMarkerB] = useState();
+  const [pointA, setPointA] = useState([150, 0]);
+  const [pointB, setPointB] = useState([150, 100]);
+  const [line, setLine] = useState();
 
   useEffect(() => {
     if (!map) setMap(initMap({ mapId, image }));
-  }, []);
+    else {
+      setMarkerA(
+        addMarker({
+          map,
+          latLng: pointA,
+          draggable: true,
+          setPosition: setPointA,
+        })
+      );
+      setMarkerB(
+        addMarker({
+          map,
+          latLng: pointB,
+          draggable: true,
+          setPosition: setPointB,
+        })
+      );
+      setLine(addPolyLine({ map, latLngs: [pointA, pointB] }));
+    }
+  }, [map]);
+
+  useEffect(() => {
+    if (map) {
+      line.remove();
+
+      setLine(addPolyLine({ map, latLngs: [pointA, pointB] }));
+    }
+  }, [pointA, pointB]);
 
   return (
     <div className="map-wrapper">
