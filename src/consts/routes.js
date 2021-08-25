ed13beb1bf327c9ea421461e701f6a7eaae08318
deadpooll312 <deadpoll312@gmail.com>
2021-08-25 12:29:22 +0600

    show map

--- src/consts/routes.js
+++ src/consts/routes.js
@@ -1,8 +1,10 @@
 import Authorization from '../pages/Authorization';
 import Main from '../pages/Main';
 import UploadMap from '../pages/UploadMap';
+import ShowMap from '../pages/ShowMap/ShowMap';
 
 export const UPLOAD_MAP = '/upload/map';
+export const SHOW_MAP = '/show/map';
 export const LOGIN = '/login';
 export const MAIN = '/';
 
@@ -24,5 +26,11 @@ export const ROUTES = [
     exact: true,
     path: UPLOAD_MAP,
     component: UploadMap,
+  },
+  {
+    isPrivate: true,
+    exact: true,
+    path: SHOW_MAP,
+    component: ShowMap,
   },
 ];
